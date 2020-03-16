f29a525a03cbfb29bbd3ff35b13a66a68d89ef25
Darkzy <tomames360@hotmail.co.uk>
2020-03-16 12:48:00 +0000

    Update server.lua

--- drp_core/admin/server.lua
+++ drp_core/admin/server.lua
@@ -2,7 +2,7 @@
 --- Functions
 ---------------------------------------------------------------------------
 function DoesRankHavePerms(rank, perm)
-    local playerPerms = DRPCoreConfig.StaffRanks.perms[rank]
+    local playerPerms = string.lower(DRPCoreConfig.StaffRanks.perms[rank])
     for a = 1, #playerPerms do
         if string.lower(playerPerms[a]) == perm then
             return true
