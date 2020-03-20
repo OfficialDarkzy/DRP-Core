ccfea9b28cf1cf4e7563eb0e433528c4edf420bd
Shredxt <47064656+shredxt@users.noreply.github.com>
2020-03-20 19:52:16 +0000

    Update voip.lua

--- drp_core/managers/voip.lua
+++ drp_core/managers/voip.lua
@@ -1,25 +1,26 @@
-local voice = {normal = 12.0, whisper = 5.0, shout = 25.0, current = 0, level = "Normal"}
+if DRPCoreConfig.Voip then
 
+  local voice = {normal = 12.0, whisper = 5.0, shout = 25.0, current = 0, level = "Normal"}
 
-AddEventHandler("onClientMapStart", function()
+  AddEventHandler("onClientMapStart", function()
     NetworkSetTalkerProximity(voice.normal)
-end)
+  end)
 
-function drawLevel(r, g, b, a)
-  SetTextFont(4)
-  SetTextProportional(1)
-  SetTextScale(0.4, 0.4)
-  SetTextColour(r, g, b, a)
-  SetTextDropShadow(0, 0, 0, 0, 255)
-  SetTextEdge(1, 0, 0, 0, 255)
-  SetTextDropShadow()
-  SetTextOutline()
-  SetTextEntry("STRING")
-  AddTextComponentString("Range: "..voice.level)
-  DrawText(0.175, 0.90)
-end
-      
-Citizen.CreateThread(function()
+  function drawLevel(r, g, b, a)
+    SetTextFont(4)
+    SetTextProportional(1)
+    SetTextScale(0.4, 0.4)
+    SetTextColour(r, g, b, a)
+    SetTextDropShadow(0, 0, 0, 0, 255)
+    SetTextEdge(1, 0, 0, 0, 255)
+    SetTextDropShadow()
+    SetTextOutline()
+    SetTextEntry("STRING")
+    AddTextComponentString("Range: "..voice.level)
+    DrawText(0.175, 0.90)
+  end
+        
+  Citizen.CreateThread(function()
     while true do
       Citizen.Wait(1)
       if IsControlPressed(1, 137) and IsControlJustPressed(1, 74) then
@@ -37,11 +38,13 @@ Citizen.CreateThread(function()
           print("voice shout")
           voice.level = "Shout"
         end
+      end
+      if NetworkIsPlayerTalking(PlayerId()) then
+        drawLevel(0,255,0,0.3)
+      elseif not NetworkIsPlayerTalking(PlayerId()) then
+        drawLevel(0, 140, 255,0.4)
+      end
     end
-    if NetworkIsPlayerTalking(PlayerId()) then
-      drawLevel(0,255,0,0.3)
-    elseif not NetworkIsPlayerTalking(PlayerId()) then
-      drawLevel(0, 140, 255,0.4)
-    end
-  end
-end)
+  end)
+  
+end
