50197605a43f42bbf5837e9122f9b286443fce0e
Shredxt <47064656+shredxt@users.noreply.github.com>
2020-03-20 20:18:55 +0000

    Revert "Update client.lua"
    This reverts commit b37923894c808e3f8fa64bfca14b168d2fdef99d.

--- drp_core/client.lua
+++ drp_core/client.lua
@@ -11,16 +11,6 @@ AddEventHandler('onClientMapStart', function()
 	TriggerServerEvent("DRP_TimeSync:ConnectionSetTime")
 end)
 
-AddEventHandler("playerSpawned", function()
-    Citizen.CreateThread(function()
-
-      local playerPed = PlayerPedId()
-
-      NetworkSetFriendlyFireOption(true)
-      SetCanAttackFriendly(playerPed, true, true)
-    end)
-end)
-
 ---------------------------------------------------------------------------
 --- You Can Edit The Below To Your Requirements, 
 --  only touch if you know what you are doing,
@@ -42,43 +32,35 @@ Citizen.CreateThread(function()
     while true do
 --------------------Disable Health Regeneration----------------------------
         SetPlayerHealthRechargeMultiplier(PlayerId(), 0.0)
+------------------------------Enable PVP-----------------------------------	
+        SetCanAttackFriendly(GetPlayerPed(PlayerId()), true, false)
+        NetworkSetFriendlyFireOption(true)
 ------------------------------Remove Police Audio--------------------------	
         DisablePoliceReports()
 ----------------------Hiding Hud Components--------------------------------
-      	if IsHudComponentActive(1) then 
-            HideHudComponentThisFrame(1)  -- Wanted Stars
-        end
-			
-        if IsHudComponentActive(2) then 
-            HideHudComponentThisFrame(2) -- Weapon icon
-        end
-			
-        if IsHudComponentActive(3) then 
-            HideHudComponentThisFrame(3) -- Cash
-        end
-			
-        if IsHudComponentActive(4) then
-            HideHudComponentThisFrame(4) -- MP CASH
-        end
+        HideHudComponentThisFrame(1) -- Wanted Stars
+        HideHudComponentThisFrame(2) -- Weapon icon
+        HideHudComponentThisFrame(3) -- Cash
+        HideHudComponentThisFrame(4) -- MP CASH
 
         if not DRPCoreConfig.Crosshair then
-            if IsHudComponentActive(14) then
-                HideHudComponentThisFrame(14) -- Cross Hair
-            end
+            HideHudComponentThisFrame(14) -- Cross Hair
         end
 ----------------------Remove Wanted Level----------------------------------
-        if GetPlayerWantedLevel(PlayerId()) ~= 0 then
-            SetPlayerWantedLevel(PlayerId(), 0, false)
-            SetPlayerWantedLevelNow(PlayerId(), false)
-        end
+        ClearPlayerWantedLevel(PlayerId())
+        SetMaxWantedLevel(0)
+        SetPoliceIgnorePlayer(PlayerId(), true)
 ------------------Controller For PED and VEHICLE density!------------------
         SetVehicleDensityMultiplierThisFrame(0.2)
         SetPedDensityMultiplierThisFrame(0.2)
         SetRandomVehicleDensityMultiplierThisFrame(0.1)
+
 --------------Remove Getting Weapons From Vehicles--------------------------
         DisablePlayerVehicleRewards(PlayerId())
+
 --------------Stop Pistol/Gun Whipping--------------------------------------
         DisableControlAction(0, 140, true)
+
 ----------------Remove Drops------------------------------------------------		
         RemoveAllPickupsOfType(0x550447A9)
         RemoveAllPickupsOfType(0xF92F486C)
