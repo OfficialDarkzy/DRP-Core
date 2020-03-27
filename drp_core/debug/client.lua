2c602b956ed598ccd68814578c234abcb8814e9d
ShiftyOG <51722240+ShiftyOG@users.noreply.github.com>
2020-03-27 14:19:20 -0600

    Player speed to vehicle speed
    When you enter a vehicle it changes the debug text from player speed to vehicle speed in MPH.

--- drp_core/debug/client.lua
+++ drp_core/debug/client.lua
@@ -5,6 +5,7 @@ if DRPCoreConfig.Debug then
     Citizen.CreateThread(function()
         while true do
             Citizen.Wait(0)
+            local player = PlayerPedId()
             x, y, z = table.unpack(GetEntityCoords(GetPlayerPed(-1), true))
             
             roundx = tonumber(string.format("%.2f", x))
@@ -19,29 +20,32 @@ if DRPCoreConfig.Debug then
             roundh = tonumber(string.format("%.2f", heading))
             DrawTxt("~r~Heading:~s~ "..roundh, 0.40, 0.05)
 
-            local rx,ry,rz = table.unpack(GetEntityRotation(PlayerPedId(), 1))
+            local rx,ry,rz = table.unpack(GetEntityRotation(player, 1))
             DrawTxt("~r~RX:~s~ "..tonumber(string.format("%.2f", rx)), 0.33, 0.1)
             DrawTxt("~r~RY:~s~ "..tonumber(string.format("%.2f", ry)), 0.44, 0.1)
             DrawTxt("~r~RZ:~s~ "..tonumber(string.format("%.2f", rz)), 0.55, 0.1)
         
-            speed = GetEntitySpeed(PlayerPedId())
+            speed = GetEntitySpeed(player)
             rounds = tonumber(string.format("%.2f", speed))
-            DrawTxt("~r~Player Speed: ~s~"..rounds, 0.40, 0.90)
 
-            health = GetEntityHealth(PlayerPedId())
+            health = GetEntityHealth(player)
             DrawTxt("~r~Player Health: ~s~"..health, 0.40, 0.85)
 
-            veheng = GetVehicleEngineHealth(GetVehiclePedIsUsing(PlayerPedId()))
-            vehbody = GetVehicleBodyHealth(GetVehiclePedIsUsing(PlayerPedId()))
-            if IsPedInAnyVehicle(PlayerPedId(), 1) then
+            veheng = GetVehicleEngineHealth(GetVehiclePedIsUsing(player))
+            vehbody = GetVehicleBodyHealth(GetVehiclePedIsUsing(player))
+            if IsPedInAnyVehicle(player, 1) then
                 vehenground = tonumber(string.format("%.2f", veheng))
                 vehbodround = tonumber(string.format("%.2f", vehbody))
+
+                DrawTxt("~r~Vehicle Speed: ~s~"..math.ceil(rounds*2.23694).."~r~MPH~s~", 0.40, 0.90)
 
                 DrawTxt("~r~Engine Health: ~s~"..vehenground, 0.0, 0.73)
 
                 DrawTxt("~r~Body Health: ~s~"..vehbodround, 0.0, 0.69)
 
-                DrawTxt("~r~Vehicle Fuel: ~s~"..tonumber(string.format("%.2f", GetVehicleFuelLevel(GetVehiclePedIsUsing(PlayerPedId())))), 0.0, 0.65)
+                DrawTxt("~r~Vehicle Fuel: ~s~"..tonumber(string.format("%.2f", GetVehicleFuelLevel(GetVehiclePedIsUsing(player)))), 0.0, 0.65)
+            else
+                DrawTxt("~r~Player Speed: ~s~"..rounds, 0.40, 0.90)
             end
         end
     end)
