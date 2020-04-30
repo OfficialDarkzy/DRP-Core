2c419f008fd316f8a748b3d393eb1bbdcb706256
Darkzy <tomames360@hotmail.co.uk>
2020-04-30 23:45:57 +0100

    Changes

--- drp_core/managers/managers.lua
+++ drp_core/managers/managers.lua
@@ -12,37 +12,36 @@ end)
 --- Functions
 ---------------------------------------------------------------------------
 function GetPlayers()
-	local players = {}
-	for i=0, DRPCoreConfig.TotalPlayersAllowed, 1 do
-		local ped = GetPlayerPed(i)
-		if DoesEntityExist(ped) then
-			table.insert(players, i)
-		end
-	end
-	return players
+    local players = {}
+
+    for i = 0, 31 do
+        if NetworkIsPlayerActive(i) then
+            table.insert(players, i)
+        end
+    end
+
+    return players
 end
 ---------------------------------------------------------------------------
 function GetClosestPlayer()
 	local players = GetPlayers()
 	local closestDistance = -1
 	local closestPlayer = -1
-    local ped = PlayerPedId()
-    local playerId = PlayerId()
+	local ply = PlayerPedId()
 	local plyCoords = GetEntityCoords(ply, 0)
 	
-	for i=1, #players, 255 do
-		local target = GetPlayerPed(players[i])
-
-		if usePlayerPed and players[i] ~= playerId then
-			local targetCoords = GetEntityCoords(target)
-			local dist = GetDistanceBetweenCoords(targetCoords, coords.x, coords.y, coords.z, true)
-
-			if closestDistance == -1 or closestDistance > dist then
-				closestPlayer   = players[i]
-				closestDistance = dist
+	for index,value in ipairs(players) do
+		local target = GetPlayerPed(value)
+		if(target ~= ply) then
+			local targetCoords = GetEntityCoords(GetPlayerPed(value), 0)
+			local distance = Vdist(targetCoords["x"], targetCoords["y"], targetCoords["z"], plyCoords["x"], plyCoords["y"], plyCoords["z"])
+			if(closestDistance == -1 or closestDistance > distance) then
+				closestPlayer = value
+				closestDistance = distance
 			end
 		end
 	end
+	
 	return closestPlayer, closestDistance
 end
 exports("GetClosestPlayer", GetClosestPlayer)
