a632a45ab6d658326e9ca167173001360ddc3ad5
Darkzy <tomames360@hotmail.co.uk>
2020-09-20 12:55:17 +0100

    PERFORMANCE :car: @shredxt

--- drp_core/client.lua
+++ drp_core/client.lua
@@ -5,6 +5,31 @@ local isPlayerReady = false
 ---------------------------------------------------------------------------
 --- Core Functions Dont EDIT BROOO ITS MY NAME CAMONNNN GIMME CREDIIIIIITTTTTTTTTTTTT
 ---------------------------------------------------------------------------
+function AddTextEntry(key, value)
+	Citizen.InvokeNative(`ADD_TEXT_ENTRY`, key, value)
+end
+
+Citizen.CreateThread(function()
+    AddTextEntry('PM_SCR_MAP', DRPCoreConfig.ESCMenu["MAP"])
+    AddTextEntry('PM_SCR_STA', DRPCoreConfig.ESCMenu["STATUS"])
+    AddTextEntry('PM_SCR_GAM', DRPCoreConfig.ESCMenu["GAME"])
+    AddTextEntry('PM_SCR_INF', DRPCoreConfig.ESCMenu["INFO"])
+    AddTextEntry('PM_SCR_SET', DRPCoreConfig.ESCMenu["SETTINGS"])
+    AddTextEntry('PM_SCR_RPL', DRPCoreConfig.ESCMenu["R*EDITOR"])
+    while true do
+        Citizen.Wait(35)
+        N_0xb9449845f73f5e9c("SHIFT_CORONA_DESC")
+        PushScaleformMovieFunctionParameterBool(true)
+        PopScaleformMovieFunction()
+        N_0xb9449845f73f5e9c("SET_HEADER_TITLE")
+        PushScaleformMovieFunctionParameterString(DRPCoreConfig.ESCMenu["TITLE"])
+        PushScaleformMovieFunctionParameterBool(true)
+        PushScaleformMovieFunctionParameterString(DRPCoreConfig.ESCMenu["SUBTITLE"])
+        PushScaleformMovieFunctionParameterBool(true)
+        PopScaleformMovieFunctionVoid()    
+    end
+end)
+
 Citizen.CreateThread(function()
     SetNuiFocus(false, false)
     TriggerServerEvent("DRP_Core:AddPlayerToTable")
@@ -19,15 +44,20 @@ AddEventHandler("playerSpawned", function()
     TriggerServerEvent("DRP_WeatherSync:ConnectionSetWeather")
     TriggerServerEvent("DRP_TimeSync:ConnectionSetTime")
     Citizen.CreateThread(function()
-      local playerPed = PlayerPedId()
-      NetworkSetFriendlyFireOption(true)
-      SetCanAttackFriendly(playerPed, true, true)
+        local playerPed = PlayerPedId()
+        NetworkSetFriendlyFireOption(true) -- Enable Friendly Fire
+        SetCanAttackFriendly(playerPed, true, true) -- Enable Friendly Fire
+        SetMaxWantedLevel(0) -- Set Max Wanted Level to 0
+        SetCreateRandomCops(0) -- Prevent AI Cop Creation
+        SetCreateRandomCopsNotOnScenarios(0) -- Prevent AI Cop Creation
+        SetCreateRandomCopsOnScenarios(0) -- Prevent AI Cop Creation
+        SetPlayerHealthRechargeLimit(PlayerId(), 0) -- Disable Health Recharge
+        SetPedSuffersCriticalHits(playerPed, false) -- Disable Critical Hits
+        SetPedMinGroundTimeForStungun(playerPed, 6000) -- Time spent on ground after being tased (in ms)
+        SetPedConfigFlag(playerPed, 184, true) -- Disable Seat Shuffle
+        SetPedConfigFlag(playerPed, 35, false) -- Disable Automatic Bike Helmet
     end)
 end)
----------------------------------------------------------------------------
-function AddTextEntry(key, value)
-	Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), key, value)
-end
 ---------------------------------------------------------------------------
 --- Core Thread
 ---------------------------------------------------------------------------
@@ -44,17 +74,10 @@ Citizen.CreateThread(function()
             EndTextCommandSetBlipName(item.blip)
         end
     end
-    AddTextEntry('PM_SCR_MAP', DRPCoreConfig.ESCMenu["MAP"])
-    AddTextEntry('PM_SCR_STA', DRPCoreConfig.ESCMenu["STATUS"])
-    AddTextEntry('PM_SCR_GAM', DRPCoreConfig.ESCMenu["GAME"])
-    AddTextEntry('PM_SCR_INF', DRPCoreConfig.ESCMenu["INFO"])
-    AddTextEntry('PM_SCR_SET', DRPCoreConfig.ESCMenu["SETTINGS"])
-    AddTextEntry('PM_SCR_RPL', DRPCoreConfig.ESCMenu["R*EDITOR"])
     while true do
---------------------Disable Health Regeneration----------------------------
-        SetPlayerHealthRechargeMultiplier(PlayerId(), 0.0)
 ----------------------Remove Police Audio----------------------------------	
         CancelCurrentPoliceReport()
+        DistantCopCarSirens(false)
 ----------------------Hiding Hud Components--------------------------------
       	if IsHudComponentActive(1) then 
             HideHudComponentThisFrame(1)  -- Wanted Stars
@@ -77,48 +100,27 @@ Citizen.CreateThread(function()
                 HideHudComponentThisFrame(14) -- Cross Hair
             end
         end
-----------------------Remove Wanted Level----------------------------------
-        if GetPlayerWantedLevel(PlayerId()) ~= 0 then
-            SetPlayerWantedLevel(PlayerId(), 0, false)
-            SetPlayerWantedLevelNow(PlayerId(), false)
-        end
 ------------------Controller For PED and VEHICLE density!------------------
         SetVehicleDensityMultiplierThisFrame(0.2)
         SetPedDensityMultiplierThisFrame(0.2)
         SetRandomVehicleDensityMultiplierThisFrame(0.1)
---------------Remove Getting Weapons From Vehicles--------------------------
-        DisablePlayerVehicleRewards(PlayerId())
 --------------Stop Pistol/Gun Whipping--------------------------------------
         DisableControlAction(0, 140, true)
 ---------------------------------------------------------------------------
-        N_0xb9449845f73f5e9c("SHIFT_CORONA_DESC")
-        PushScaleformMovieFunctionParameterBool(true)
-        PopScaleformMovieFunction()
-        N_0xb9449845f73f5e9c("SET_HEADER_TITLE")
-        PushScaleformMovieFunctionParameterString(DRPCoreConfig.ESCMenu["TITLE"])
-        PushScaleformMovieFunctionParameterBool(true)
-        PushScaleformMovieFunctionParameterString(DRPCoreConfig.ESCMenu["SUBTITLE"])
-        PushScaleformMovieFunctionParameterBool(true)
-        PopScaleformMovieFunctionVoid()    
-        Citizen.Wait(1)
+        Citizen.Wait(5)
     end
 end)
 ---------------------------------------------------------------------------
 --- Prevent NPCs from dropping weapons when dead
 ---------------------------------------------------------------------------
-local hashes = {
-    0xA9355DCD, -- PUMP SHOTGUN
-    0xDF711959, -- CARBINE RIFLE
-    0xF9AFB48F -- PISTOL
-}
-----------------------------------------------------------------------------
-Citizen.CreateThread(function()
-    if DRPCoreConfig.RemovePedWeaponDrops then
-        while true do
-        Citizen.Wait(1)
-            for k,v in pairs(hashes) do
-                RemoveAllPickupsOfType(v)
-            end
-        end
+function RemoveWeaponDrops()
+    local pickupList = {`PICKUP_AMMO_BULLET_MP`,`PICKUP_AMMO_FIREWORK`,`PICKUP_AMMO_FLAREGUN`,`PICKUP_AMMO_GRENADELAUNCHER`,`PICKUP_AMMO_GRENADELAUNCHER_MP`,`PICKUP_AMMO_HOMINGLAUNCHER`,`PICKUP_AMMO_MG`,`PICKUP_AMMO_MINIGUN`,`PICKUP_AMMO_MISSILE_MP`,`PICKUP_AMMO_PISTOL`,`PICKUP_AMMO_RIFLE`,`PICKUP_AMMO_RPG`,`PICKUP_AMMO_SHOTGUN`,`PICKUP_AMMO_SMG`,`PICKUP_AMMO_SNIPER`,`PICKUP_ARMOUR_STANDARD`,`PICKUP_CAMERA`,`PICKUP_CUSTOM_SCRIPT`,`PICKUP_GANG_ATTACK_MONEY`,`PICKUP_HEALTH_SNACK`,`PICKUP_HEALTH_STANDARD`,`PICKUP_MONEY_CASE`,`PICKUP_MONEY_DEP_BAG`,`PICKUP_MONEY_MED_BAG`,`PICKUP_MONEY_PAPER_BAG`,`PICKUP_MONEY_PURSE`,`PICKUP_MONEY_SECURITY_CASE`,`PICKUP_MONEY_VARIABLE`,`PICKUP_MONEY_WALLET`,`PICKUP_PARACHUTE`,`PICKUP_PORTABLE_CRATE_FIXED_INCAR`,`PICKUP_PORTABLE_CRATE_UNFIXED`,`PICKUP_PORTABLE_CRATE_UNFIXED_INCAR`,`PICKUP_PORTABLE_CRATE_UNFIXED_INCAR_SMALL`,`PICKUP_PORTABLE_CRATE_UNFIXED_LOW_GLOW`,`PICKUP_PORTABLE_DLC_VEHICLE_PACKAGE`,`PICKUP_PORTABLE_PACKAGE`,`PICKUP_SUBMARINE`,`PICKUP_VEHICLE_ARMOUR_STANDARD`,`PICKUP_VEHICLE_CUSTOM_SCRIPT`,`PICKUP_VEHICLE_CUSTOM_SCRIPT_LOW_GLOW`,`PICKUP_VEHICLE_HEALTH_STANDARD`,`PICKUP_VEHICLE_HEALTH_STANDARD_LOW_GLOW`,`PICKUP_VEHICLE_MONEY_VARIABLE`,`PICKUP_VEHICLE_WEAPON_APPISTOL`,`PICKUP_VEHICLE_WEAPON_ASSAULTSMG`,`PICKUP_VEHICLE_WEAPON_COMBATPISTOL`,`PICKUP_VEHICLE_WEAPON_GRENADE`,`PICKUP_VEHICLE_WEAPON_MICROSMG`,`PICKUP_VEHICLE_WEAPON_MOLOTOV`,`PICKUP_VEHICLE_WEAPON_PISTOL`,`PICKUP_VEHICLE_WEAPON_PISTOL50`,`PICKUP_VEHICLE_WEAPON_SAWNOFF`,`PICKUP_VEHICLE_WEAPON_SMG`,`PICKUP_VEHICLE_WEAPON_SMOKEGRENADE`,`PICKUP_VEHICLE_WEAPON_STICKYBOMB`,`PICKUP_WEAPON_ADVANCEDRIFLE`,`PICKUP_WEAPON_APPISTOL`,`PICKUP_WEAPON_ASSAULTRIFLE`,`PICKUP_WEAPON_ASSAULTSHOTGUN`,`PICKUP_WEAPON_ASSAULTSMG`,`PICKUP_WEAPON_AUTOSHOTGUN`,`PICKUP_WEAPON_BAT`,`PICKUP_WEAPON_BATTLEAXE`,`PICKUP_WEAPON_BOTTLE`,`PICKUP_WEAPON_BULLPUPRIFLE`,`PICKUP_WEAPON_BULLPUPSHOTGUN`,`PICKUP_WEAPON_CARBINERIFLE`,`PICKUP_WEAPON_COMBATMG`,`PICKUP_WEAPON_COMBATPDW`,`PICKUP_WEAPON_COMBATPISTOL`,`PICKUP_WEAPON_COMPACTLAUNCHER`,`PICKUP_WEAPON_COMPACTRIFLE`,`PICKUP_WEAPON_CROWBAR`,`PICKUP_WEAPON_DAGGER`,`PICKUP_WEAPON_DBSHOTGUN`,`PICKUP_WEAPON_FIREWORK`,`PICKUP_WEAPON_FLAREGUN`,`PICKUP_WEAPON_FLASHLIGHT`,`PICKUP_WEAPON_GRENADE`,`PICKUP_WEAPON_GRENADELAUNCHER`,`PICKUP_WEAPON_GUSENBERG`,`PICKUP_WEAPON_GOLFCLUB`,`PICKUP_WEAPON_HAMMER`,`PICKUP_WEAPON_HATCHET`,`PICKUP_WEAPON_HEAVYPISTOL`,`PICKUP_WEAPON_HEAVYSHOTGUN`,`PICKUP_WEAPON_HEAVYSNIPER`,`PICKUP_WEAPON_HOMINGLAUNCHER`,`PICKUP_WEAPON_KNIFE`,`PICKUP_WEAPON_KNUCKLE`,`PICKUP_WEAPON_MACHETE`,`PICKUP_WEAPON_MACHINEPISTOL`,`PICKUP_WEAPON_MARKSMANPISTOL`,`PICKUP_WEAPON_MARKSMANRIFLE`,`PICKUP_WEAPON_MG`,`PICKUP_WEAPON_MICROSMG`,`PICKUP_WEAPON_MINIGUN`,`PICKUP_WEAPON_MINISMG`,`PICKUP_WEAPON_MOLOTOV`,`PICKUP_WEAPON_MUSKET`,`PICKUP_WEAPON_NIGHTSTICK`,`PICKUP_WEAPON_PETROLCAN`,`PICKUP_WEAPON_PIPEBOMB`,`PICKUP_WEAPON_PISTOL`,`PICKUP_WEAPON_PISTOL50`,`PICKUP_WEAPON_POOLCUE`,`PICKUP_WEAPON_PROXMINE`,`PICKUP_WEAPON_PUMPSHOTGUN`,`PICKUP_WEAPON_RAILGUN`,`PICKUP_WEAPON_REVOLVER`,`PICKUP_WEAPON_RPG`,`PICKUP_WEAPON_SAWNOFFSHOTGUN`,`PICKUP_WEAPON_SMG`,`PICKUP_WEAPON_SMOKEGRENADE`,`PICKUP_WEAPON_SNIPERRIFLE`,`PICKUP_WEAPON_SNSPISTOL`,`PICKUP_WEAPON_SPECIALCARBINE`,`PICKUP_WEAPON_STICKYBOMB`,`PICKUP_WEAPON_STUNGUN`,`PICKUP_WEAPON_SWITCHBLADE`,`PICKUP_WEAPON_VINTAGEPISTOL`,`PICKUP_WEAPON_WRENCH`, `PICKUP_WEAPON_RAYCARBINE`}
+    local player = PlayerId()
+    for a = 1, #pickupList do
+		N_0x616093ec6b139dd9(player, pickupList[a], false)
     end
+end
+
+Citizen.CreateThread(function()     
+    RemoveWeaponDrops()
 end)
